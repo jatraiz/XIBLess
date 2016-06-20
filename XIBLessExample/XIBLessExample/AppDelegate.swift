cc9eb7596162bb628f18c03e6719286b24578d69
John L. Stricker <john.stricker@raizlabs.com>
2016-06-20 15:55:56 -0500

    created workspace

---
+++ XIBLessExample/XIBLessExample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  XIBLessExample
+//
+//  Created by John Stricker on 6/20/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+
+}
+
